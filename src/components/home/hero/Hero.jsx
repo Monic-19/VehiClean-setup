17c3551a9e0739a23b32bc883651ee9c4d93d3a5
monic vyas <118365854+Monic-19@users.noreply.github.com>
2025-09-14 17:22:52 +0530

    corrected pnpm issue

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -7,12 +7,19 @@ const Hero = () => {
   return (
     <div className='hero -mt-3'>
       <ReactLenis root />
+      
       <section className="intro">
         <h1 className='uppercase absolute top-4 left-4 md:top-8 md:left-8 lg:top-5 lg:left-5'>Our Services <br />at a Glance</h1>
         <p className='absolute bottom-8 right-9 md:bottom-16 md:right-16 lg:bottom-12 lg:right-14 tracking-tight scale-200 '>(&darr;)</p>
       </section>
 
       <Stickycards />
+      
+      <div className='headline uppercase p-5 flex flex-col justify-center mb-[15vh]'>
+        <h1>Drive Clean, Spend Less </h1>
+        <h1 className='lg:ml-32'>Explore Our Service Bundles</h1>
+      </div>
+
 
       <section className="outro">
         <h1>Ends the form</h1>
